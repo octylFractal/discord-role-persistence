cd9aa419b42d210b732f6f92c1d8ed219d389848
Kenzie Togami <ket1999@gmail.com>
2018-04-18 12:10:28 -0700

    Allow role mappings

---
+++ src/db.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import {Snowflake} from "discord.js";
+import lowdb from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+
+const adapter = new FileSync('db.json');
+const db = lowdb(adapter);
+
+db.defaults({
+    roleMap: {},
+    roleMappings: {},
+    admins: [],
+    token: ''
+}).write();
+
+export function setUserRoles(uid: Snowflake, gid: Snowflake, roles: Snowflake[]) {
+    db.set(['roleMap', gid, uid], roles).write();
+}
+
+export function getUserRoles(uid: Snowflake, gid: Snowflake): Snowflake[] {
+    return db.get(['roleMap', gid, uid]).value();
+}
+
+export function setRoleMapping(gid: Snowflake, fromId: Snowflake, toId: Snowflake) {
+    db.set(['roleMappings', gid, fromId], toId).write();
+}
+
+export function getRoleMapppings(gid: Snowflake): {[k: string]: string} {
+    return db.get(['roleMappings', gid]).value();
+}
+
+export function getAdmins(): Snowflake[] {
+    return db.get('admins').value();
+}
+
+export function getToken(): string {
+    return db.get('token').value();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
 #!/usr/bin/env node
-import Discord, {Guild, GuildMember, Snowflake} from "discord.js";
-import lowdb from "lowdb";
-import FileSync from "lowdb/adapters/FileSync";
+import Discord, {Guild, GuildMember, Message} from "discord.js";
+import {getAdmins, getRoleMapppings, getToken, getUserRoles, setRoleMapping, setUserRoles} from "./db";
 
 const client = new Discord.Client();
-
-const adapter = new FileSync('db.json');
-const db = lowdb(adapter);
-
-db.defaults({
-    roleMap: {}
-});
-
-function setUserRoles(uid: Snowflake, gid: Snowflake, roles: Snowflake[]) {
-    db.set(['roleMap', gid, uid], roles).write();
-}
-
-function getUserRoles(uid: Snowflake, gid: Snowflake): Snowflake[] {
-    return db.get(['roleMap', gid, uid]).value();
-}
 
 function getMemTag(member: GuildMember) {
     return `[${member.id}:${member.user.username}@${member.guild.id}:${member.guild.name}]`;
@@ -26,12 +10,7 @@ function getMemTag(member: GuildMember) {
 
 function setRolesFromGuildMember(member: GuildMember) {
     const memTag = getMemTag(member);
-    // The role with an ID === guild ID is the @everyone role
-    // Don't try to hand it out!
-    // Also don't save managed roles...
-    const roles = member.roles.array()
-        .filter(r => r.id !== member.guild.id)
-        .filter(r => !r.managed);
+    const roles = member.roles.array();
     console.log(memTag, 'Saving roles:', roles.map(r => ({
         id: r.id,
         name: r.name
@@ -60,10 +39,20 @@ client.on('guildMemberAdd', member => {
         return;
     }
     console.log(memTag, 'Found some roles, applying...');
-    // update individually?
+    const roleMap = getRoleMapppings(member.guild.id);
     roles.forEach(r => {
+        if (roleMap[r]) {
+            console.log(memTag, 'Mapping role', r, 'to', roleMap[r]);
+            r = roleMap[r];
+        }
+        let roleObj = member.guild.roles.get(r);
+        // exclude dropped roles, @everyone, non-existent roles, and managed roles.
+        if (r === '0' || r === member.guild.id || typeof roleObj === "undefined" || roleObj.managed) {
+            console.log(memTag, 'Dropping role', r);
+            return;
+        }
         member.addRole(r, 'role-persistence: user joined, adding saved roles.')
-            .then(() => console.log(memTag, 'Applied!'))
+            .then(() => console.log(memTag, 'Applied', r))
             .catch(err => console.error(memTag, 'Failed to add role:', err));
     });
 });
@@ -72,5 +61,91 @@ client.on('guildMemberUpdate', (old, member) => {
     setRolesFromGuildMember(member);
 });
 
-client.login('NDM2MDY5MzM5MjgwNDQxMzQ0.DbiIyA._-JkV8Np_hwAluhrvHajvDtbWBs')
+function isAdminDm(message: Message) {
+    return message.channel.type === 'dm' && getAdmins().indexOf(message.author.id) >= 0;
+}
+
+function replyMessage(message: Message, reply: string) {
+    message.reply(reply)
+        .catch(err => console.warn('Error sending reply', err));
+}
+
+type Command = (message: Message, argv: string[]) => void;
+
+const commands: { [k: string]: Command } = {
+    maprole(message: Message, argv: string[]) {
+        if (argv.length <= 3) {
+            replyMessage(message, 'Error: 3 arguments required.');
+            return;
+        }
+        const [gid, from, to] = argv.slice(1);
+        const guild = client.guilds.get(gid);
+        if (typeof guild === "undefined") {
+            replyMessage(message, "Error: bot does not exist in guild");
+            return;
+        }
+        const roles = guild.roles;
+        if (!roles.has(from)) {
+            replyMessage(message, "Error: `from` role does not exist in guild");
+            return;
+        }
+        if (!roles.has(to) && to !== '0') {
+            replyMessage(message, "Error: `to` role does not exist in guild");
+            return;
+        }
+        setRoleMapping(gid, from, to);
+        replyMessage(message, "Mapped roles successfully!");
+    },
+    guilds(message: Message) {
+        replyMessage(message, 'Guilds:');
+        replyMessage(message, client.guilds.sort((a, b) => a.name.localeCompare(b.name)).map(g => `${g.name} (${g.id})`).join('\n'));
+    },
+    roles(message: Message, argv) {
+        if (argv.length <= 1) {
+            replyMessage(message, "Error: 1 argument required.");
+            return;
+        }
+        const gid = argv[1];
+        const guild = client.guilds.get(gid);
+        if (typeof guild === "undefined") {
+            replyMessage(message, "Error: bot does not exist in guild");
+            return;
+        }
+        replyMessage(message, 'Roles:');
+        replyMessage(message, guild.roles.sort((a, b) => a.name.localeCompare(b.name)).map(r => `${r.name} (${r.id})`).join('\n'));
+    },
+    help(message) {
+        replyMessage(message, 'Commands:');
+        for (let k of Object.keys(commands)) {
+            replyMessage(message, '!' + k);
+        }
+    }
+};
+
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) {
+        // don't reply-self
+        return;
+    }
+    if (!isAdminDm(message)) {
+        replyMessage(message, `Oh hai ${message.author.username}!`);
+        return;
+    }
+    const text = message.content;
+    if (!text.startsWith('!')) {
+        replyMessage(message, `Good day, mistress ${message.author.username}.`);
+        return;
+    }
+    const argv = text.substring(1).split(' ');
+    console.log('EXEC', argv);
+    const cmd = commands[argv[0]];
+    if (typeof cmd === "undefined") {
+        replyMessage(message, "Error: unknown command.");
+        return;
+    }
+    cmd(message, argv);
+});
+
+client.login(getToken())
     .catch(e => console.error('log in error!', e));
